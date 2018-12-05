4b41a043fd6b0aa293a8d65d2ddbfbce76acc22c
Dimen <a4gkyum@naver.com>
2018-12-05 16:45:18 +0900

    형태소분석 적용

--- outdated/preprocess.py
+++ outdated/preprocess.py
@@ -1,22 +1,34 @@
 from random import shuffle
+from konlpy.tag import Kkma
 
-f = open("review_neg.txt", 'r', encoding='utf-8')
-g = open("review_pos.txt", 'r', encoding='utf-8')
-h = open("kkk.train", 'w', encoding='utf-8')
+f = open("outdated/review_neg_dev.txt", 'r')
+g = open("outdated/review_pos_dev.txt", 'r')
+h = open("outdated/kkk.dev", 'w')
+
+kkma = Kkma()
+
+
+def load_data_and_labels(sentence):
+    li = [i[0] for i in kkma.pos(sentence)]
+    my_str = ""
+    for i in li:
+        my_str += (i + ' ')
+    return my_str
+
 
 for i in f.readlines():
-    h.write(i.strip() + '|neg\n')
+    h.write(load_data_and_labels(i) + '|neg\n')
 for j in g.readlines():
-    h.write(j.strip() + '|pos\n')
-
+    h.write(load_data_and_labels(j) + '|pos\n')
 
 h.close()
-h = open("kkk.train", 'r', encoding='utf-8')
+
+h = open("outdated/kkk.dev", 'r')
 sentences = h.readlines()
 shuffle(sentences)
 
 h.close()
-h = open("kkk.train", 'w', encoding='utf-8')
+h = open("outdated/kkk.dev", 'w')
 
 for sentence in sentences:
     h.write(str(sentence))
